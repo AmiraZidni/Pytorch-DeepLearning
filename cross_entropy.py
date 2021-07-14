023f76aa3f28fb4960ea123a0775ce65bc7d587c
Zidni Imani <zidni.imani@gmail.com>
2021-07-14 14:54:13 +0700

    edit

--- cross_entropy.py
+++ cross_entropy.py
@@ -7,5 +7,5 @@ def cross_entropy(Y, P):
     P = np.float_(P)
     return -np.sum(Y * np.log(P)+ (1 - Y) * np.log(1 - P))
 
-print(cross_entropy([1,0,1,1]))
+print(cross_entropy([1,0,1,1],[0.4,0.6,0.1,0.5]))
 
